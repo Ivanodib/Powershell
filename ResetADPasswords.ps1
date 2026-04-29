23d731f42fd097b4af63385bf89e072ec9904a16
Ivanodib <38322663+Ivanodib@users.noreply.github.com>
2026-04-29 14:43:34 +0200

    Update ResetADPasswords.ps1

--- ResetADPasswords.ps1
+++ ResetADPasswords.ps1
@@ -1,3 +1,4 @@
+
 param (
     [string]$File
 )
@@ -15,7 +16,6 @@ function New-RandomPassword {
     $special = '!@#$%*-_'
     $all     = ($lower + $upper + $digits + $special).ToCharArray()
 
-    # garantisce complessità minima
     $pwd = @(
         ($lower.ToCharArray()   | Get-Random -Count 1)
         ($upper.ToCharArray()   | Get-Random -Count 1)
@@ -23,29 +23,44 @@ function New-RandomPassword {
         ($special.ToCharArray() | Get-Random -Count 1)
     )
 
-    # completa fino a 12 caratteri
     $pwd += (1..8 | ForEach-Object { $all | Get-Random })
 
-    # shuffle finale
     return (-join ($pwd | Get-Random -Count $pwd.Count))
 }
 
 $Users = Import-Csv -Path $File
 
+$Results = @()
+
 foreach ($user in $Users) {
-    $upn = $user.UPN.Trim()
-    if ($upn) {
-        try {
-            $pwd = New-RandomPassword
-            $sec = ConvertTo-SecureString $pwd -AsPlainText -Force
 
-            Set-ADAccountPassword -Identity $upn -NewPassword $sec -Reset
+    $upn = $user.UPN
+    $sam = $user.UPN.Split('@')[0]
 
-            Write-Host "[OK] $upn | password reset" -ForegroundColor Green
-        }
-        catch {
-            Write-Host "[ERR] $upn - $_" -ForegroundColor Red
-        }
+    $ResultObj = [PSCustomObject]@{
+        UPN    = $upn
+        SAM    = $sam
+        Result = ""
     }
+
+    try {
+        $pwd = New-RandomPassword
+        $sec = ConvertTo-SecureString $pwd -AsPlainText -Force
+
+        Set-ADAccountPassword -Identity $sam -NewPassword $sec -Reset -ErrorAction Stop
+
+        $ResultObj.Result = "password resetted"
+        Write-Host "[OK] $upn | password resetted" -ForegroundColor Green
+    }
+    catch {
+        $ResultObj.Result = $_.Exception.Message
+        Write-Host "[ERR] $upn - $_.Exception.Message" -ForegroundColor Red
+    }
+
+    $Results += $ResultObj
 }
-``
+
+$OutFile = "C:\temp\account-resetted.csv"
+
+$Results | Export-Csv -Path $OutFile -NoTypeInformation -Encoding UTF8
+Write-Host "Output salvato in $OutFile" -ForegroundColor Green
